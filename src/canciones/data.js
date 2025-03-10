5f118b3afcb2d863a2a5aedfb916124fbd58a889
undajesusdavid <undajesusdavid@gmail.com>
2025-03-09 20:17:02 -0400

    Agredando mas canciones

--- src/canciones/data.js
+++ src/canciones/data.js
@@ -13,5 +13,42 @@ export const canciones = [
         tipo: "Adoración",
         link: "https://www.youtube.com/watch?v=JOqX6Y9ByC4",
     },
+    {
+        id: 3,
+        nombre : "A donde huiré",
+        interprete: "Kabed",
+        tipo: "Adoración",
+        link: "https://www.youtube.com/watch?v=UzZiutBTcbU",
+    },
+    {
+        id: 4,
+        nombre : "Aceite Fresco",
+        interprete: "New Wine",
+        tipo: "Adoración",
+        link: "https://www.youtube.com/watch?v=p_bY_SPtQQg",
+    },
+    {
+        id: 5,
+        nombre : "Al estar aquí",
+        interprete: "Danilo Montero",
+        tipo: "Adoración",
+        link: "https://www.youtube.com/watch?v=-G3EcW8iaTk",
+    },
+    {
+        id: 6,
+        nombre : "Al que está sentado en el trono",
+        interprete: "Marcos Brunet Ft Lucas Conslie",
+        tipo: "Adoración",
+        link: "https://www.youtube.com/watch?v=mYQqmiUA9ps",
+    },
+    {
+        id: 7,
+        nombre : "Algo esta pasando",
+        interprete: "Oasis Ministry",
+        tipo: "Adoración",
+        link: "https://www.youtube.com/watch?v=jw0oO0d63MQ",
+    },
+
+
    
 ] 
